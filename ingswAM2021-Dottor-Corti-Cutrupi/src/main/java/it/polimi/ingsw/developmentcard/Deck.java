1f0875e739c2efefbe6f32aa337deea976faa05e
Sam <samuele1.corti@mail.polimi.it>
2021-03-22 17:47:16 +0100

    DevelopmentCard featuress added; some methods still to test.

---
+++ ingswAM2021-Dottor-Corti-Cutrupi/src/main/java/it/polimi/ingsw/developmentcard/Deck.java
@@ -0,0 +1,24 @@
+package it.polimi.ingsw.developmentcard;
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Deck {
+    private List <DevelopmentCard> deck;
+    public void addNewCard(DevelopmentCard cardToAdd){
+        this.deck.add(cardToAdd);
+    }
+    public DevelopmentCard getFirstCard(){
+        return this.deck.get(0);
+    }
+    public void shuffle(){
+        Collections.shuffle(this.deck);
+    }
+    public DevelopmentCard drawCard(){
+        DevelopmentCard temp=this.deck.get(0);
+        this.deck.remove(0);
+        return temp;
+    }
+}
