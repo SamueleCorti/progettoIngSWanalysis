2660f9adaaddeedbfd2b407587fe1a3d5a4b8d21
Sam <samuele1.corti@mail.polimi.it>
2021-04-04 17:55:39 +0200

    DevelopmentCardDeck updated to have methods to get the color and the level, and to remove a card.

--- ingswAM2021-Dottor-Corti-Cutrupi/src/main/java/it/polimi/ingsw/developmentcard/DevelopmentCardDeck.java
+++ ingswAM2021-Dottor-Corti-Cutrupi/src/main/java/it/polimi/ingsw/developmentcard/DevelopmentCardDeck.java
@@ -6,25 +6,51 @@ import java.util.Collections;
 import java.util.List;
 
 public class DevelopmentCardDeck {
+    /* all the cards in the deck are supposed to be of the same tier and of the same color; this
+    requisite must be used during the instantiation of the deck */
     private List <DevelopmentCard> deck;
-    public DevelopmentCardDeck(){
-        this.deck = new ArrayList<DevelopmentCard>();
+    private Color deckColor;
+    private int deckLevel;
+
+    public DevelopmentCardDeck(List<DevelopmentCard> deck, Color deckColor, int deckLevel) {
+        this.deck = deck;
+        this.deckColor = deckColor;
+        this.deckLevel = deckLevel;
     }
+
+    public Color getDeckColor(){
+        return this.deckColor;
+    }
+
+    public int getDeckLevel() {
+        return deckLevel;
+    }
+
+
     public void addNewCard(DevelopmentCard cardToAdd){
         this.deck.add(cardToAdd);
     }
+
     public DevelopmentCard getFirstCard(){
         return this.deck.get(0);
     }
+
     public void shuffle(){
         Collections.shuffle(this.deck);
     }
+
     public DevelopmentCard drawCard(){
         DevelopmentCard temp=this.deck.get(0);
         this.deck.remove(0);
         return temp;
     }
+
     public int deckSize(){
         return this.deck.size();
     }
+
+    public void removeCard(){
+        this.deck.remove(0);
+    }
+
 }
