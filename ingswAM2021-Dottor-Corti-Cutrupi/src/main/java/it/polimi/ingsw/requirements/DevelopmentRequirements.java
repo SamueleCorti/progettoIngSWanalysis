62e1211afd6d8262de438ec83628907c58288eaa
Sam <samuele1.corti@mail.polimi.it>
2021-04-11 16:47:53 +0200

    added comments; fixed the checkRequirement of DevelopmentRequirements class (not tested)

--- ingswAM2021-Dottor-Corti-Cutrupi/src/main/java/it/polimi/ingsw/requirements/DevelopmentRequirements.java
+++ ingswAM2021-Dottor-Corti-Cutrupi/src/main/java/it/polimi/ingsw/requirements/DevelopmentRequirements.java
@@ -2,15 +2,20 @@ package it.polimi.ingsw.requirements;
 
 import it.polimi.ingsw.Dashboard;
 import it.polimi.ingsw.developmentcard.Color;
+import it.polimi.ingsw.developmentcard.DevelopmentCard;
 import it.polimi.ingsw.developmentcard.DevelopmentCardZone;
 import it.polimi.ingsw.requirements.Requirements;
 import org.javatuples.Pair;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /*
 this class represents the requirements of leader cards that require active development cards in the dashboard
  */
 
 public class DevelopmentRequirements implements Requirements {
+    // number of cards with the specified level and color that i need to have
     private Integer amountOfDevelopmentRequired;
     private Integer levelRequired;
     private Color colorRequired;
@@ -43,16 +48,27 @@ public class DevelopmentRequirements implements Requirements {
     }
 
     public boolean checkRequirement(Dashboard dashboard){
-        //IN THIS WAY IT DOESNT WORK, IT STILL HAS TO BE IMPLEMENTED
-        /*
-        for(DevelopmentCardZone developmentCardZone: dashboard.getDevelopmentCardZones()){
-            for(Pair<Integer,Color> cardStats: developmentCardZone.getCardsStats()){
-                //not 100% sure if "==" is allowed for pairs. This has to be tested
-                if(developmentRequired==cardStats){
-                    return true;
+        //we create a temporary list of all the cards in the dashboard where we check the requirements
+        List <DevelopmentCard> developmentCardsInDashboard = new ArrayList<DevelopmentCard>();
+        //here we populate the list
+            for (DevelopmentCardZone developmentCardZone : dashboard.getDevelopmentCardZones()) {
+                for(DevelopmentCard developmentCardToAdd : developmentCardZone.getCards()){
+                    developmentCardsInDashboard.add(developmentCardToAdd);
                 }
             }
-        }*/
-        return false;
+        /* we now check if there's a number of cards of the required level and color;
+        we use the temporary int i as a counter of their occurrences in the list */
+        int i=0;
+        for(DevelopmentCard developmentCard: developmentCardsInDashboard){
+            if(developmentCard.getCardStats().getValue1()==colorRequired && developmentCard.getCardStats().getValue0()>=levelRequired){
+                i = i++;
+            }
+        }
+        //if the counter equals the number of required cards, we return true
+        if(i==this.amountOfDevelopmentRequired) {
+            return true;
+        }else{
+            return false;
+        }
     }
 }
