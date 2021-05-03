2074162edc6d01a2fa5ad9fba924b48e34f5ec65
MarcoDottor <marco.dottor@mail.polimi.it>
2021-05-03 17:02:08 +0200

    added Game

---
+++ ingswAM2021-Dottor-Corti-Cutrupi/src/main/java/it/polimi/ingsw/Game.java
@@ -0,0 +1,11 @@
+package it.polimi.ingsw;
+
+import java.util.ArrayList;
+
+public class Game {
+    ArrayList<Player> players;
+
+    public ArrayList<Player> getPlayers() {
+        return players;
+    }
+}
