6ad710ba5f5719e6a5a5dd1b62a84758752d64b6
MarcoDottor <marco.dottor@mail.polimi.it>
2021-05-28 18:27:52 +0200

    Created dashboard's fxml file

---
+++ ingswAM2021-Dottor-Corti-Cutrupi/src/main/java/it/polimi/ingsw/client/gui/controllers/DashboardController.java
@@ -0,0 +1,15 @@
+package it.polimi.ingsw.client.gui.controllers;
+
+import it.polimi.ingsw.client.gui.GUI;
+import javafx.fxml.FXML;
+
+import javax.swing.text.TableView;
+import javax.swing.text.html.ImageView;
+
+public class DashboardController implements GUIController{
+
+    @Override
+    public void setGui(GUI gui) {
+
+    }
+}
