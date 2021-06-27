e2968afd5d0f153bc1fff23c060e52b83285959e
SamueleCorti <80049956+SamueleCorti@users.noreply.github.com>
2021-06-27 11:50:32 +0200

    deleted modifier classes and tester from project

--- ingswAM2021-Dottor-Corti-Cutrupi/src/main/java/it/polimi/ingsw/server/messages/jsonMessages/DashboardMessage.java
+++ ingswAM2021-Dottor-Corti-Cutrupi/src/main/java/it/polimi/ingsw/server/messages/jsonMessages/DashboardMessage.java
@@ -27,37 +27,5 @@ public class DashboardMessage implements Message {
             System.out.println("the dashboard is" + jsonDashboard);
         }
     }
-    /*private Warehouse warehouse;
-    private Strongbox strongbox;
-    private LeaderCardZone leaderCardZone;
-    private ArrayList<DevelopmentCardZone> developmentCardZones;
-    private PapalPath papalPath;
-    private ArrayList<ExtraDepot> extraDepots;
-    private ArrayList<Resource> whiteToColorResources;
-    private ArrayList<Resource> discountedResources;
-    //resources that represent the extra productions brought by the Leader Power
-    private ArrayList<Resource> resourcesForExtraProd;
-    //resources produced in this turn, at the end of the turn they will be moved in the strongbox
-    private ArrayList <Resource> resourcesProduced;
-    //number of resources consumed by the standard prod
-    private int numOfStandardProdRequirements;
-    //number of resources produced by the standard prod
-    private int numOfStandardProdResults;
-
-    public DashboardMessage(Dashboard dashboard) {
-        this.warehouse = dashboard.getWarehouse();
-        this.strongbox = dashboard.getStrongbox();
-        this.leaderCardZone = dashboard.getLeaderCardZone();
-        this.developmentCardZones = dashboard.getDevelopmentCardZones();
-        this.papalPath = dashboard.getPapalPath();
-        this.extraDepots = dashboard.getExtraDepots();
-        this.whiteToColorResources = dashboard.getWhiteToColorResources();
-        this.discountedResources = dashboard.getDiscountedResources();
-        this.resourcesForExtraProd = dashboard.getResourcesForExtraProd();
-        this.resourcesProduced = dashboard.getResourcesProduced();
-        this.numOfStandardProdRequirements = dashboard.getNumOfStandardProdRequirements();
-        this.numOfStandardProdResults = dashboard.getNumOfStandardProdResults();
-    }*/
-
 
 }
